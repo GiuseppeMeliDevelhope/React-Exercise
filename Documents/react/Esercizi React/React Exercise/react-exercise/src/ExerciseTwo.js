406d6def3f957d934a9cc96b3c9b4a4cbe2ff877
Giuseppe Meli <meligiuseppe92020@gmail.com>
2023-02-22 00:36:19 +0100

    exercise-2

---
+++ Documents/react/Esercizi React/React Exercise/react-exercise/src/ExerciseTwo.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export class ExerciseTwo extends React.Component{
+    render(){
+        return(
+            <div>
+                <h1>Hello, {this.props.name}.</h1>
+            </div>
+        )
+    }
+}
+
