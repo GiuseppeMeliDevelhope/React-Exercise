9f644c1eb8a2595fc7722e6d26b97b413ced03db
Giuseppe Meli <meligiuseppe92020@gmail.com>
2023-02-22 12:41:05 +0100

    exercise-22

---
+++ Documents/react/Esercizi React/React Exercise/react-exercise/src/Counter.js
@@ -0,0 +1,34 @@
+import React from "react";
+import { CounterDisplay } from "./CounterDisplay";
+
+export class Counter extends React.Component{
+    state= {
+        count:  this.props.initialValue
+    }
+
+    constructor(props){
+        super(props)
+       this._interval= setInterval(()=> {
+         this.setState((state) => {
+            return {
+               count: state.count + this.props.increment
+            }
+         })
+        }, this.props.intervalTime)
+    }
+      
+    render(){
+    
+       return <CounterDisplay count={this.state.count}/>
+   
+    }
+
+    
+}
+
+Counter.defaultProps={
+    initialValue:0,
+    increment:1,
+    intervalTime:1000
+
+}
