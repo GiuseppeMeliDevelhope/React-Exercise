5f1b22ee5164c0ba6a15dde555b7b930e02af948
Giuseppe Meli <meligiuseppe92020@gmail.com>
2023-02-22 13:07:04 +0100

    exercise-34

---
+++ Documents/react/Esercizi React/React Exercise/react-exercise/src/TodoList.js
@@ -0,0 +1,45 @@
+import React from "react";
+
+export class TodoList extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      items: []
+    }
+
+    this.handleAddItem = this.handleAddItem.bind(this)
+  }
+
+  handleAddItem() {
+    const newItem = this.inputElement.value;
+
+    if (newItem !== "") {
+      this.setState((prevState) => ({
+        items: [...prevState.items, newItem]
+      }))
+      
+      //now it clears the input field after the value is added to the item array
+      this.inputElement.value = "";
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <ul>
+          {this.state.items.map((item, index) => (
+            <li key={index}>{item}</li>
+          ))}
+        </ul>
+        <div>
+          <input type="text" ref={(a) => (this.inputElement = a)} />
+          <button onClick={this.handleAddItem}>Add</button>
+          <button onClick={()=>this.setState({items:[]})}>Reset</button>
+        </div>
+      </div>
+    )
+  }
+}
+
+
