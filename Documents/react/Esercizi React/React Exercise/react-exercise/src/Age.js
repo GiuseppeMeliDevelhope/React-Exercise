62bb56cb7aefad4b1c78ac5ba9508c5e3711a4d8
Giuseppe Meli <meligiuseppe92020@gmail.com>
2023-02-22 12:33:30 +0100

    exercise-17

---
+++ Documents/react/Esercizi React/React Exercise/react-exercise/src/Age.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export class Age extends React.Component{
+    render(){
+       if(this.props.age>18 && this.props.age<65){
+        return <p>Your age is {this.props.age}.</p>
+       }
+    }
+}
+
+//Enter here your age:
+//The age has to be a number
+Age.defaultProps= {
+    age:22
+ }
