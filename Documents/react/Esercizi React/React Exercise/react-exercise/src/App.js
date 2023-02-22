f97038487ac7673bcff3d1e013b2ee23b9b11802
Giuseppe Meli <meligiuseppe92020@gmail.com>
2023-02-22 12:37:33 +0100

    exercise-20

---
+++ Documents/react/Esercizi React/React Exercise/react-exercise/src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { Counter } from "./Counter";
+import { Hello } from "./Hello";
+import { Welcome } from "./Welcome";
+
+export class App extends React.Component{
+    render(){
+        return(
+            <div>
+                <Hello />
+                <Welcome />
+                <Counter />
+            </div>
+        )
+    }
+}
