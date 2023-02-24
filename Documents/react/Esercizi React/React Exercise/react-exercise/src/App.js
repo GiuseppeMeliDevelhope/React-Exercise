c70e6bf3561921b364e285156e4513e130734e08
Giuseppe Meli <meligiuseppe92020@gmail.com>
2023-02-24 15:39:05 +0100

    exercise-46

---
+++ Documents/react/Esercizi React/React Exercise/react-exercise/src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { ClickCounter } from "./ClickCounter";
+
+
+export class App extends React.Component{
+    
+    
+    render(){
+        return(
+            <div>
+               
+                <ClickCounter />
+               
+            </div>
+        )
+    }
+}
