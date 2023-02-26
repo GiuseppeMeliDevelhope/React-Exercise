ac0eae288fac6f4d9f73f870395349640d6dcc12
Giuseppe Meli <meligiuseppe92020@gmail.com>
2023-02-26 02:18:36 +0100

    exercise-50

---
+++ Documents/react/Esercizi React/React Exercise/react-exercise/src/GithubUserLogic.js
@@ -0,0 +1,36 @@
+import { useState } from "react"
+ 
+
+export function GithubUserLogic(){
+    const[data, setData]= useState(null)
+    const[loading, setLoading]= useState(false)
+    const [error, setError]= useState(null)
+     
+    
+   async function fetchGithubUser(username) {
+     setLoading(true)
+         setError(null)
+    
+      try{
+         const response =await fetch(`https://api.github.com/users/${username}`)
+           const json = await response.json()
+             
+          if (response.status !== 200){
+            setError(new Error(''))
+          }
+              setData(json)
+              console.log(json)
+           }catch (error) {
+            setError(error)
+            setData(null)
+           
+         }finally {
+             setLoading(false)
+         }
+          
+   }  
+         
+    
+
+     return{ data, error, loading, onFetchUser: fetchGithubUser}
+}
